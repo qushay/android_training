43acaf4b969158d19f7e2f4a2ae72f1b7052d6b0
qushay <bagas.qushay@gmail.com>
2016-09-15 23:09:42 +0700

    profile map

--- app/src/main/java/com/test/androidtrainingtest/fragment/TwoFragment.java
+++ app/src/main/java/com/test/androidtrainingtest/fragment/TwoFragment.java
@@ -10,24 +10,62 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.test.androidtrainingtest.R;
 
-public class TwoFragment extends Fragment{
+public class TwoFragment extends Fragment {
 
-    public TwoFragment() {
-        // Required empty public constructor
-    }
+    private GoogleMap mGoogleMap;
+    private MapView mMapView;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public TwoFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mGoogleMap = mMap;
+                LatLng pnm = new LatLng(-6.205385,106.8192383);
+                mGoogleMap.addMarker(new MarkerOptions().position(pnm).title("Marker in Sydney"));
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pnm,16));
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 
 }
