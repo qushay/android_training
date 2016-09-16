2f0eb30548d8571b16f368213715c02e5c27b8c4
qushay <bagas.qushay@gmail.com>
2016-09-17 01:18:47 +0700

    profile call & sms

--- app/src/main/java/com/test/androidtrainingtest/fragment/OneFragment.java
+++ app/src/main/java/com/test/androidtrainingtest/fragment/OneFragment.java
@@ -4,12 +4,15 @@ package com.test.androidtrainingtest.fragment;
  * Created by Qushay on 15/09/2016.
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.test.androidtrainingtest.R;
@@ -17,6 +20,8 @@ import com.test.androidtrainingtest.R;
 public class OneFragment extends Fragment{
 
     TextView tvNama, tvTglLahir, tvTempatLahir, tvAlamat, tvPekerjaan;
+    Button btCall, btSms;
+    String mNumber = "081327219704";
 
     public OneFragment() {
         // Required empty public constructor
@@ -32,11 +37,33 @@ public class OneFragment extends Fragment{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_one, container, false);
+
+        btCall = (Button) view.findViewById(R.id.call);
+        btSms = (Button) view.findViewById(R.id.sms);
+
         tvNama = (TextView) view.findViewById(R.id.nama);
         tvTglLahir = (TextView) view.findViewById(R.id.tgl_lahir);
         tvTempatLahir = (TextView) view.findViewById(R.id.tempat_lahir);
         tvAlamat = (TextView) view.findViewById(R.id.alamat);
         tvPekerjaan = (TextView) view.findViewById(R.id.pekerjaan);
+
+        btCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.fromParts("tel", mNumber, null));
+                startActivity(intent);
+            }
+        });
+
+        btSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.fromParts("sms", mNumber, null));
+                startActivity(intent);
+            }
+        });
 
         tvNama.setText(Html.fromHtml("Nama <b>Don Juans</b>"));
         tvTglLahir.setText(Html.fromHtml("Tgl Lahir <b>31 Febuari 1945</b>"));
