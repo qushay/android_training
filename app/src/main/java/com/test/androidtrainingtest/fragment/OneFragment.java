fd12c658010c7cdcda3b1389a603539c506e8d55
qushay <bagas.qushay@gmail.com>
2016-09-17 00:27:06 +0700

    profile tab profile

--- app/src/main/java/com/test/androidtrainingtest/fragment/OneFragment.java
+++ app/src/main/java/com/test/androidtrainingtest/fragment/OneFragment.java
@@ -6,13 +6,17 @@ package com.test.androidtrainingtest.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.test.androidtrainingtest.R;
 
 public class OneFragment extends Fragment{
+
+    TextView tvNama, tvTglLahir, tvTempatLahir, tvAlamat, tvPekerjaan;
 
     public OneFragment() {
         // Required empty public constructor
@@ -26,8 +30,21 @@ public class OneFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        // Inflate the layout for this fragment // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        tvNama = (TextView) view.findViewById(R.id.nama);
+        tvTglLahir = (TextView) view.findViewById(R.id.tgl_lahir);
+        tvTempatLahir = (TextView) view.findViewById(R.id.tempat_lahir);
+        tvAlamat = (TextView) view.findViewById(R.id.alamat);
+        tvPekerjaan = (TextView) view.findViewById(R.id.pekerjaan);
+
+        tvNama.setText(Html.fromHtml("Nama <b>Don Juans</b>"));
+        tvTglLahir.setText(Html.fromHtml("Tgl Lahir <b>31 Febuari 1945</b>"));
+        tvTempatLahir.setText(Html.fromHtml("Tempat Lahir <b>Jakarta</b>"));
+        tvAlamat.setText(Html.fromHtml("Alamat <b>Jl. Merdeka Barat No 1, Jakarta</b>"));
+        tvPekerjaan.setText(Html.fromHtml("Pekerjaan <b>Programmer</b>"));
+
+        return view;
     }
 
 }
